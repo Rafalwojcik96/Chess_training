4b330e76fa2a6e05cf88393b83270900b2df39ec
Rafal Wojcik <xire2@o2.pl>
2021-02-03 12:40:25 +0100

    draw board

--- training.js
+++ training.js
@@ -1,12 +1,20 @@
 const checkerboards = [
 	{image:"nf6.jpg",answer:"nf6"},
-	{image:"qa4.jpg",answer:"qa4"}
+	{image:"qa4.jpg",answer:"qa4"},
+	{image:"sicilian_defense.jpg",answer:"nc3"}
 ];
 
+const array_number = checkerboards[Math.floor(Math.random()*checkerboards.length)];
+
+window.onload = draw;
+
+function draw(){
+	$("#exercise").html(`<img src="img/${array_number.image}">`)
+}
 
 function checkmate(){
-	let correct_answer = checkerboards[0].answer;
-	let player_answer = $('#box').val();
+	const correct_answer = array_number.answer;
+	const player_answer = $('#box').val();
 	if (player_answer == correct_answer)
 	{
 		$('.answer').html("That's right! It's best move.")
