1350acd2fd8bd9035c8e26b17b678281ea128697
Rafal Wojcik <xire2@o2.pl>
2021-02-04 13:22:40 +0100

    added the possibility to play more

--- training.js
+++ training.js
@@ -2,10 +2,10 @@ const checkerboards = [
 	{image:"nf6.jpg",answer:"nf6", color:"black"},
 	{image:"qa4.jpg",answer:"qa4",color:"white"},
 	{image:"sicilian_defense.jpg",answer:"nc3",color:"white"},
-	{image:"nd3.jpg",answer:"nb3",color:"black"},
+	{image:"nd3.jpg",answer:"nd3",color:"black"},
 ];
 
-const array_number = checkerboards[Math.floor(Math.random()*checkerboards.length)];
+let array_number = checkerboards[Math.floor(Math.random()*checkerboards.length)];
 
 window.onload = draw;
 
@@ -36,12 +36,46 @@ function change_mark(){
 function checkmate(){
 	const correct_answer = array_number.answer;
 	const player_answer = $('#box').val();
-	if (player_answer == correct_answer)
-	{
-		$('.answer').html("That's right! It's best move.")
+	if (player_answer == correct_answer){	
+		$('.answer').html(`
+		<div class="correct_answer">
+			<div class="label">
+				<label for="box">Correct!</label>
+			</div>
+			<input type="submit" value="Try next!" onclick="next()">
+		</div>`)
 	}
-	else
-	{
-		$(".answer").html ("It's not best move.")
+	else{	
+		$(".answer").html(`
+		<div class="correct_answer">
+			<div class="label">
+				<label for="box">It's not the best move.</label>
+			</div>
+			<input type="submit" value="Try again!" onclick="again()">
+		</div>`);
 	}
+}
+
+function again(){
+	$('.answer').html(`
+	<div class="label">
+		<label for="box">Find the best move</label>
+	</div>
+	<input type="text" id="box" autocomplete="off">
+	<div id="button">
+		<input type="submit" value="Check!" onclick="checkmate()">
+	</div>`)
+}
+
+function next(){
+	array_number=checkerboards[Math.floor(Math.random()*checkerboards.length)];
+	draw();
+	$(".answer").html(`
+	<div class="label">
+		<label for="box">Find the best move</label>
+	</div>
+	<input type="text" id="box" autocomplete="off">
+	<div id="button">
+		<input type="submit" value="Check!" onclick="checkmate()">
+	</div>`)
 }
