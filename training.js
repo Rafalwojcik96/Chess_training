bfc2bdefa1537a776a4560bb7ab59ca536eccd24
Rafal Wojcik <xire2@o2.pl>
2021-02-04 12:38:05 +0100

    swap table to JS

--- training.js
+++ training.js
@@ -23,6 +23,14 @@ function change_mark(){
 		<li>h</li><li>g</li><li>f</li><li>e</li><li>d</li><li>c</li><li>b</li><li>a</li>
 	</ol`);
 	}
+	else {
+		$(".chessboard_mark_numbers").html(`<ul>
+		<li>8</li><li>7</li><li>6</li><li>5</li><li>4</li><li>3</li><li>2</li><li>1</li>
+	</ul>`)
+		$(".chessboard_mark").html(`<ol>
+		<li>a</li><li>b</li><li>c</li><li>d</li><li>e</li><li>f</li><li>g</li><li>h</li>
+	</ol>`)
+	}
 }
 
 function checkmate(){
